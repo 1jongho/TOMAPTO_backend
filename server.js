511cfcb29b1a50e60ca2c11af82256e541794bb1
mmmid720 <mmmid720@gmail.com>
2025-04-14 17:56:48 +0900

    프로필,로그인 백 추가, 서버 백 수정

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cors = require("cors");
 const app = express();
 const accountRoutes = require("./account/signup"); // signup.js 파일도 함께 실행
 const loginRoutes = require("./account/login"); // login.js 파일 추가
+const profileRoutes = require("./account/profile"); // profile.js 파일 추가
+const logoutRoutes = require("./account/logout"); // logout.js 파일 추가
 const db = require("./db.js"); // db.js 파일도 함께 실행
 
 // 미들웨어 설정
@@ -34,7 +36,9 @@ app.get("/api/account/signup", (res, req) => {
 
 // 회원가입 및 로그인 관련 라우트 설정
 app.use("/api/account", accountRoutes);
-app.use("/api/account/login", loginRoutes); // 경로 수정
+app.use("/api/account/login", loginRoutes); // 로그인 라우트
+app.use("/api/account/profile", profileRoutes); // 프로필 라우트
+app.use("/api/account/logout", logoutRoutes); // 로그아웃 라우트 추가
 
 // 서버 시작
 app.listen(8080, "0.0.0.0", () => {
